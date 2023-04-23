bb9a7676ede601bc589ece85ebecb432e2b10256
jaiswaladi246 <32607172+jaiswaladi246@users.noreply.github.com>
2023-04-23 18:44:06 +0530

    Update DevopsIntegrationApplication.java

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "welcome to javatechie";
+		return "WELCOME TO DEVOPS SHACK";
 	}
 
 	public static void main(String[] args) {
